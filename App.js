721dfee0dd721f72dd1c6f475d73a8a8a387b62d
Priya Mayilsamy <priyasmy@amazon.com>
2022-04-01 17:02:26 -0500

    Chapter 2 components added

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, SafeAreaView, TouchableHighlight,Button, Alert, Platform} from 'react-native';
+
 
 export default function App() {
 
   console.log("App executed")
-  let x;
-  //x.toString();
+
+  const handlePress = () => console.log("Text Pressed");
+  const handleButtonPress = () => Alert.prompt("Prompt testing", "Button pressed",(text)=> console.log(text));
   return (
-    <View style={styles.container}>
-      <Text>Hello Everyone!! This is my first mobile app.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button 
+      color= "orange"
+      title="Click Me" onPress={handleButtonPress}/>
+      <Text numberOfLines={1} onPress={handlePress}>Hello Everyone!! This is my first mobile app.Write a really really really long text just by prefixing really multiple times.</Text>
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+      <Image source={{width:200,
+        height:300,
+        uri:"https://picsum.photos/200/300"}}/>
+      </TouchableHighlight> 
+ 
+      
+    </SafeAreaView>
   );
 }
 
@@ -18,7 +29,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     alignItems: 'center',
+     justifyContent: 'center',
+
   },
 });
