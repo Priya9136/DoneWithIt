240d02e3663bf561eec0c54f8be7227883f85d57
Priya Mayilsamy <priyasmy@amazon.com>
2022-04-10 16:39:12 -0500

    Input component changes

--- App.js
+++ App.js
@@ -9,9 +9,30 @@ import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppPicker from './app/components/AppPicker';
+import Screen from './app/components/Screen';
+import AppTextInput from './app/components/AppTextInput';
+import { useState } from 'react';
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
+  const [category , setCategory] = useState();
   console.log("App executed")
- return <ListingsScreen/>;
+ return (
+   <Screen>
+     <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+     <AppTextInput icon="email" placeholder="Email" />
+   </Screen>
+ );
 }
