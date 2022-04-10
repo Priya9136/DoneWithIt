f5dea29442dc02d0ad2ed0285c5bf9046559645a
Priya Mayilsamy <priyasmy@amazon.com>
2022-04-10 16:03:32 -0500

    Lists changes

--- App.js
+++ App.js
@@ -6,9 +6,12 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
+import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 
 export default function App() {
   console.log("App executed")
- return <ViewImageScreen/>;
+ return <ListingsScreen/>;
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,39 +1,61 @@
-import React from 'react';
-import { Image, StyleSheet, View} from 'react-native';
-import App from '../../App';
-import colors from '../config/colors';
-import AppText from './AppText';
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TouchableHighlight,
+} from "react-native";
+import AppText from "./AppText";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
-function ListItem({title,subTitle,image}) {
-    return (
+import colors from "../config/colors";
+
+function ListItem({
+  title,
+  subTitle,
+  image,
+  IconComponent,
+  onPress,
+  renderRightActions,
+}) {
+  return (
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-            <Image style={styles.image} source={image}/>
-            <View>
-                <AppText style={styles.title}>{title}</AppText>
-                <AppText style={styles.subTitle}>{subTitle}</AppText>
-            </View>
+          {IconComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+          </View>
         </View>
-    );
+      </TouchableHighlight>
+    </Swipeable>
+  );
 }
 
 const styles = StyleSheet.create({
-
-    container:{
-        flexDirection: 'row'
-    },
-    image:{
-        width: 70,
-        height: 70,
-        borderRadius: 35,
-        marginRight: 10
-    },
-    title :{
-        fontWeight: "500"
-    },
-    subTitle:{
-        color: colors.medium
-    }
-    
-})
+  container: {
+    flexDirection: "row",
+    padding: 15,
+    backgroundColor: colors.white,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+  subTitle: {
+    color: colors.medium,
+  },
+  title: {
+    fontWeight: "500",
+  },
+});
 
 export default ListItem;
